5edb895e746fcff5c7fbcec200e254f1ca5d1ff8
Gwada33 <gwadagwada22@gmail.com>
2022-09-10 15:24:53 -0400

    test

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js')
-const Clien = new Discord.Client({
+const Client = new Discord.Client({
   intents: [
     Discord.Intents.FLAGS.GUILDS, 
   ],
 })
 
-bot.on('ready', function () {
+Client.on('ready', function () {
   console.log("Je suis connecté !")
 })
 
 
-bot.login("MTAxNzkxNzg0MDcxMzU5Njk3OA.GJlYT_.IXR9dEgiFVkfYcF-YLrDvH0hHXRKkuUBUeHOAU")
+Client.login("MTAxNzkxNzg0MDcxMzU5Njk3OA.GJlYT_.IXR9dEgiFVkfYcF-YLrDvH0hHXRKkuUBUeHOAU")
